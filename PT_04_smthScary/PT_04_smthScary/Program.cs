ed489df28991d97cabe40bbf8d8492884c34ff8e
Oleg OI <oimod.team@gmail.com>
2024-03-16 17:37:26 +0200

    alphabet to numbers

---
+++ PT_04_smthScary/PT_04_smthScary/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PT_04_smthScary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] engAlphabet = new char[26];
+            for (int i = 0; i < engAlphabet.Length; i++)
+                engAlphabet[i] = (char)(i + 'a');
+            foreach (char i in engAlphabet)
+                Console.WriteLine($"Letter \"{i}\" ASCII is {(int)i}");
+        }
+    }
+}
