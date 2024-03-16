e21e57dcff49b0cae9537ed4c593692aed4daa64
Oleg OI <oimod.team@gmail.com>
2024-03-16 17:36:37 +0200

    textSymbols but no search logic

---
+++ PT_03_textSybmolsWthSpaces_no logic/PT_03_textSybmolsWthSpaces/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PT_03_textSybmolsWthSpaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbSpaces = 0;
+            var symbNoSpaces = 0;
+
+            Console.WriteLine("Write here anything: ");
+            string givenText = Console.ReadLine();
+            while (string.IsNullOrEmpty(givenText))
+            {
+                Console.WriteLine("You need to write at least ONE symbol!\nTry Again:");
+                givenText = Console.ReadLine();
+            }
+
+            Console.WriteLine("___________________\n");
+
+            foreach (char i in givenText)
+            {
+                if (i == ' ')
+                    symbSpaces++;
+                else
+                {
+                    symbNoSpaces++;
+                    symbSpaces++;
+                }
+            }
+
+            Console.WriteLine($"Text \"{givenText}\" include: \n" +
+                $"- {symbSpaces} symbols with spaces\n" +
+                $"- {symbNoSpaces} symbols without spaces");
+            Console.WriteLine("________________\n");
+
+        }
+    }
+}
