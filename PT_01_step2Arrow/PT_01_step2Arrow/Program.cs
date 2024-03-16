3ce99d336dbf4cf4a410fca01ef83fcc0e7e34b2
Oleg OI <oimod.team@gmail.com>
2024-03-16 17:33:30 +0200

    step2Arrey

---
+++ PT_01_step2Arrow/PT_01_step2Arrow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PT_01_step2Arrow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+
+            for (int i = 0, j = 2; i < array.Length; i++, j += 2)
+                array[i] = j;
+
+            foreach (int i in array)
+                Console.WriteLine(i);
+
+            Console.WriteLine("__________________________\n");
+            Console.WriteLine($"Arrey length: {array.Length}");
+        }
+    }
+}
